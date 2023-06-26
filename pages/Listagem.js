ec7f387e1673fdc55a7ada75d2c21678aede68f7
Felipeinfo18 <felipe_ntakiguchi@hotmail.com>
2023-06-26 17:14:21 -0300

    add logoff button

--- pages/Listagem.js
+++ pages/Listagem.js
@@ -1,11 +1,31 @@
-import { Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
 import styles from '../styles/styles';
+import { signOut } from '@firebase/auth'
+import { getAuth } from '@firebase/auth';
+import app from '../FirebaseConfig';
 
 export default function Listagem() {
-  return (
-    <View style={styles.container}>
-        <Text>Listagem</Text>
-    </View>
-  );
+    const auth = getAuth(app)
+
+    const deslogar = () => {
+        signOut(auth)
+            .then((userCredentials) => {
+                console.log(userCredentials)
+                navigation.navigate('')
+            }).catch((err) => {
+                console.error(err)
+            })
+    }
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={{ marginTop: 15 }} onPress={deslogar}>
+                <View style={styles.button}>
+                    <Text style={styles.buttonText}>Sair</Text>
+                </View>
+            </TouchableOpacity>
+            <Text>Listagem</Text>
+        </View>
+    );
 }
 
